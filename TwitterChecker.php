9dade629fd3fcff0a860bba91835ff4297425e8a
SubRighteous <danjrys@gmail.com>
2017-11-24 23:41:35 -0500

    Now shows all tweets that have been made

--- TwitterChecker.php
+++ TwitterChecker.php
@@ -24,12 +24,18 @@ $string = json_decode($twitter->setGetfield($getfield)
    
 if($string["errors"][0]["message"] != "")
 {
-    echo "<h3>Oh, There was a problem. </h3><p>It Seems that Twitter has returned an error : </p><p><em>".$string[errors][0]["message"]."</em></p>";exit();
+    echo "<h3>Oh, There was a problem. </h3><p>It Seems that Twitter has returned an error:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();
 }
-
-echo "<pre>";
-print_r($string);
-echo "</pre>";
+foreach($string as $items)
+    {
+        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
+        echo "Tweet: ". $items['text']."<br />";
+        echo "Tweeted by: ". $items['user']['name']."<br />";
+        echo "Screen name: ". $items['user']['screen_name']."<br />";
+        echo "Followers: ". $items['user']['followers_count']."<br />";
+        echo "Friends: ". $items['user']['friends_count']."<br />";
+        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
+    }
 
 ?>
 
