030a72dbc48f47b79a0aa85afd4874c42fb7fac0
SubRighteous <danjrys@gmail.com>
2017-11-25 12:16:09 -0500

    Changed User

--- LatestTweets/tweets.php
+++ LatestTweets/tweets.php
@@ -74,10 +74,10 @@
 		$twitter_style_dates = true){           // Twitter style days. [about an hour ago] (Default : true)
 			
 		// Twitter keys (You'll need to visit https://dev.twitter.com and register to get these.
-		$consumerkey         = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
-		$consumersecret      = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
-		$accesstoken         = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
-		$accesstokensecret   = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        $consumerkey         = "QueAfQcIO2abML06Agq9EqwCi";
+		$consumersecret      = "SXGpe9PZl8n7TbNxcDcZfYqyuis8DAEn8h0w7rRTYRf8T72PWT";
+		$accesstoken         = "934196992795906048-ibkBh2c66f97XcJex9QeVFIi4vveXsK";
+		$accesstokensecret   = "ygyR3rXlDzitl5d0IFUpamZGw5NBbcQhvF3O2Jz2Kalz1";
 		
 		// Seconds to cache feed (Default : 3 minutes).
 		$cachetime           = 60*3;
@@ -198,5 +198,5 @@
 	}
 	
 	// Display latest tweets. (Modify username to your Twitter handle)
-	display_latest_tweets('andrew_biggart');
+	display_latest_tweets('RysFamilyLights');
 ?>
